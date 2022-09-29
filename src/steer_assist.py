017a4d86e220a04abb4be0bff5be5f2be6624f38
Jason K. Moore <moorepants@gmail.com>
2022-09-29 22:22:18 +0200

    Added an initial LQR implementation.

--- src/steer_assist.py
+++ src/steer_assist.py
@@ -1,4 +1,8 @@
+import os
+
 import numpy as np
+import scipy as sp
+from scipy.linalg import solve_continuous_are
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 
@@ -6,11 +10,26 @@ from bicycleparameters.bicycle import benchmark_par_to_canonical
 from bicycleparameters.models import Meijaard2007Model
 from bicycleparameters.parameter_sets import Meijaard2007ParameterSet
 
+SCRIPT_PATH = os.path.realpath(__file__)
+SRC_DIR = os.path.dirname(SCRIPT_PATH)
+ROOT_DIR = os.path.realpath(os.path.join(SRC_DIR, '..'))
+FIG_DIR = os.path.join(ROOT_DIR, 'figures')
+
 
 class SteerAssistModel(Meijaard2007Model):
     """
 
     Tdel_total = -kphidot*phidot - kphi*phi + Tdel
+
+    Tdel_total = [kphi, kdelta, kphidot, kdeltadot] * x
+
+    x = [roll angle,
+         steer angle,
+         roll rate,
+         steer rate]
+
+        The inputs are [roll torque,
+                        steer torque]
 
     """
 
@@ -31,45 +50,55 @@ class SteerAssistModel(Meijaard2007Model):
                     C1[i, 1, 0] = C1[i, 1, 0] + val
                 elif array_key == 'kphi':
                     K0[i, 1, 0] = K0[i, 1, 0] + val
+                elif array_key == 'kdeltadot':
+                    C1[i, 1, 1] = C1[i, 1, 1] + val
+                elif array_key == 'kdelta':
+                    K0[i, 1, 1] = K0[i, 1, 1] + val
                 else:
                     C1[i, 1, 0] = C1[i, 1, 0] + par['kphidot']
                     K0[i, 1, 0] = K0[i, 1, 0] + par['kphi']
+                    C1[i, 1, 1] = C1[i, 1, 1] + par['kdeltadot']
+                    K0[i, 1, 1] = K0[i, 1, 1] + par['kdelta']
             return M, C1, K0, K2
         else:
             M, C1, K0, K2 = benchmark_par_to_canonical(par)
             C1[1, 0] = C1[1, 0] + par['kphidot']
+            C1[1, 1] = C1[1, 1] + par['kdeltadot']
             K0[1, 0] = K0[1, 0] + par['kphi']
+            K0[1, 1] = K0[1, 1] + par['kdelta']
             return M, C1, K0, K2
 
-
+# NOTE : This is Browser + Jason taken from HumanControl repo
 meijaard2007_parameters = {  # dictionary of the parameters in Meijaard 2007
-    'IBxx': 9.2,
-    'IBxz': 2.4,
-    'IByy': 11.0,
-    'IBzz': 2.8,
-    'IFxx': 0.1405,
-    'IFyy': 0.28,
-    'IHxx': 0.05892,
-    'IHxz': -0.00756,
-    'IHyy': 0.06,
-    'IHzz': 0.00708,
-    'IRxx': 0.0603,
-    'IRyy': 0.12,
-    'c': 0.08,
+    'IBxx': 11.3557360401,
+    'IBxz': -1.96756380745,
+    'IByy': 12.2177848012,
+    'IBzz': 3.12354397008,
+    'IFxx': 0.0904106601579,
+    'IFyy': 0.149389340425,
+    'IHxx': 0.253379594731,
+    'IHxz': -0.0720452391817,
+    'IHyy': 0.246138810935,
+    'IHzz': 0.0955770796289,
+    'IRxx': 0.0883819364527,
+    'IRyy': 0.152467620286,
+    'c': 0.0685808540382,
     'g': 9.81,
-    'lam': np.pi/10.0,
-    'mB': 85.0,
-    'mF': 3.0,
-    'mH': 4.0,
-    'mR': 2.0,
-    'rF': 0.35,
-    'rR': 0.3,
-    'v': 2.0,
-    'w': 1.02,
-    'xB': 0.3,
-    'xH': 0.9,
-    'zB': -0.9,
-    'zH': -0.7,
+    'lam': 0.399680398707,
+    'mB': 81.86,
+    'mF': 2.02,
+    'mH': 3.22,
+    'mR': 3.11,
+    'rF': 0.34352982332,
+    'rR': 0.340958858855,
+    'v': 1.0,
+    'w': 1.121,
+    'xB': 0.289099434117,
+    'xH': 0.866949640247,
+    'zB': -1.04029228321,
+    'zH': -0.748236400835,
+    'kdelta': 0.0,
+    'kdeltadot': 0.0,
     'kphi': 0.0,
     'kphidot': 0.0,
 }
@@ -78,18 +107,42 @@ meijaard2007_parameters = {  # dictionary of the parameters in Meijaard 2007
 parameter_set = Meijaard2007ParameterSet(meijaard2007_parameters, True)
 
 model = SteerAssistModel(parameter_set)
-greys = mpl.colormaps['Greys']
-greys = mpl.colormaps['viridis']
-gains = [-1.0, -5.0, -10.0, -50.0, -100.0, -500.0, -1000.0, -5000.0]
-color_vals = np.linspace(0.2, 1.0, num=len(gains) + 1)
-ax = model.plot_eigenvalue_parts(v=np.linspace(0.0, 10.0, num=2000),
-                                 colors=[greys(color_vals[0])]*3)
-for gain, color_val in zip(gains, color_vals[1:]):
-    color_rgb = greys(color_val)
+cmap = mpl.colormaps['viridis']
+gains = [0.0, -1.0, -5.0, -10.0, -50.0, -100.0, -500.0, -1000.0, -5000.0]
+color_vals = np.linspace(0.2, 1.0, num=len(gains))
+
+fig, ax = plt.subplots()
+for gain, color_val in zip(gains, color_vals):
+    color_rgb = cmap(color_val)
     ax = model.plot_eigenvalue_parts(ax=ax, kphidot=gain,
                                      colors=(color_rgb, color_rgb, color_rgb),
                                      v=np.linspace(0.0, 10.0, num=2000))
 ax.set_ylim((-10.0, 10.0))
+
+fig.savefig(os.path.join(FIG_DIR, 'roll-rate-eig-effect.png'), dpi=300)
+
+speeds = np.linspace(0.0, 10.0, num=1000)
+A, B = model.form_state_space_matrices(v=0.5)
+B_delta = B[:, 1, np.newaxis]  # shape(4,1)
+R = np.array([1.0])
+Q = np.eye(4)
+S = sp.linalg.solve_continuous_are(A, B_delta, Q, R)
+K = np.squeeze(B_delta.T @ S)
+kphi, kdelta, kphidot, kdeltadot = K
+A_closed, _ = model.form_state_space_matrices(v=0.5,
+                                              kphi=float(kphi),
+                                              kdelta=float(kdelta),
+                                              kphidot=float(kphidot),
+                                              kdeltadot=float(kdeltadot))
+fig, ax = plt.subplots()
+ax = model.plot_eigenvalue_parts(ax=ax,
+                                 kphi=float(kphi),
+                                 kdelta=float(kdelta),
+                                 kphidot=float(kphidot),
+                                 kdeltadot=float(kdeltadot),
+                                 v=np.linspace(0.0, 10.0, num=2000))
+#A_closed = A - B[:, 1] @ K
+
 
 #model.plot_eigenvalue_parts(kphidot=np.linspace(-20.0, 10.0, num=1000))
 #model.plot_eigenvalue_parts(v=np.linspace(0.0, 10.0, num=1000))
