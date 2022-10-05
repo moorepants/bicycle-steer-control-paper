e1280b63e5e46246d1c0a93da352ea38c2618aa0
Jason K. Moore <moorepants@gmail.com>
2022-10-05 22:58:53 +0200

    Made compatible with new multiple array inputs.

--- src/steer_assist.py
+++ src/steer_assist.py
@@ -69,7 +69,7 @@ class SteerControlModel(Meijaard2007Model):
         """
         gain_names = ['kphi', 'kdelta', 'kphidot', 'kdeltadot']
 
-        par, array_key, array_val = self._parse_parameter_overrides(
+        par, array_keys, array_len = self._parse_parameter_overrides(
             **parameter_overrides)
 
         # g, v, and the contoller gains are not used in the computation of M,
@@ -79,46 +79,34 @@ class SteerControlModel(Meijaard2007Model):
             **parameter_overrides)
 
         # steer controller gains, 2x4, no roll control
-        if array_key in gain_names:
+        if any(k in gain_names for k in array_keys):
             # if one of the gains is an array, create a set of gain matrices
             # where that single gain varies across the set
             K = np.array([[0.0, 0.0, 0.0, 0.0],
-                          [par[p][0] if p == array_key else par[p]
+                          [par[p][0] if p in array_keys else par[p]
                            for p in gain_names]])
             # K is now shape(n, 2, 4)
-            K = np.tile(K, (len(array_val), 1, 1))
-            K[:, 1, gain_names.index(array_key)] = array_val
+            K = np.tile(K, (array_len, 1, 1))
+            for k in array_keys:
+                if k in gain_names:
+                    K[:, 1, gain_names.index(k)] = par[k]
         else:  # gains are not an array
             K = np.array([[0.0, 0.0, 0.0, 0.0],
                           [par[p] for p in gain_names]])
 
-        if M.ndim == 3:  # one of the canonical parameters is an array
-            A = np.zeros((M.shape[0], 4, 4))
-            B = np.zeros((M.shape[0], 4, 2))
-            for i, (Mi, C1i, K0i, K2i) in enumerate(zip(M, C1, K0, K2)):
-                Ai, Bi = ab_matrix(Mi, C1i, K0i, K2i, par['v'], par['g'])
-                A[i] = Ai - Bi@K
-                B[i] = Bi
-        elif array_key in gain_names:
-            A = np.zeros((len(array_val), 4, 4))
-            B = np.zeros((len(array_val), 4, 2))
-            for i, Ki in enumerate(K):
-                Ai, Bi = ab_matrix(M, C1, K0, K2, par['v'], par['g'])
+        if array_keys:
+            A = np.zeros((array_len, 4, 4))
+            B = np.zeros((array_len, 4, 2))
+            for i in range(array_len):
+                Mi = M[i] if M.ndim == 3 else M
+                C1i = C1[i] if C1.ndim == 3 else C1
+                K0i = K0[i] if K0.ndim == 3 else K0
+                K2i = K2[i] if K2.ndim == 3 else K2
+                vi = par['v'] if np.isscalar(par['v']) else par['v'][i]
+                gi = par['g'] if np.isscalar(par['g']) else par['g'][i]
+                Ki = K[i] if K.ndim == 3 else K
+                Ai, Bi = ab_matrix(Mi, C1i, K0i, K2i, vi, gi)
                 A[i] = Ai - Bi@Ki
-                B[i] = Bi
-        elif array_key == 'v':
-            A = np.zeros((len(par['v']), 4, 4))
-            B = np.zeros((len(par['v']), 4, 2))
-            for i, vi in enumerate(par['v']):
-                Ai, Bi = ab_matrix(M, C1, K0, K2, vi, par['g'])
-                A[i] = Ai - Bi@K
-                B[i] = Bi
-        elif array_key == 'g':
-            A = np.zeros((len(par['g']), 4, 4))
-            B = np.zeros((len(par['g']), 4, 2))
-            for i, gi in enumerate(par['g']):
-                Ai, Bi = ab_matrix(M, C1, K0, K2, par['v'], gi)
-                A[i] = Ai - Bi@K
                 B[i] = Bi
         else:  # scalar parameters
             A, B = ab_matrix(M, C1, K0, K2, par['v'], par['g'])
