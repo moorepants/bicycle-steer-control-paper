fdcb3fc699deb363089df164ee0a71355c196562
Jason K. Moore <moorepants@gmail.com>
2023-05-17 12:52:43 +0200

    Corrected the x axis on Leila's controller plot.

--- src/pd_control.py
+++ src/pd_control.py
@@ -7,6 +7,7 @@ import numpy as np
 import scipy.optimize as spo
 
 from data import bike_with_rider
+#from data import rigid_bike_without_rider as bike_with_rider
 from model import SteerControlModel
 
 SCRIPT_PATH = os.path.realpath(__file__)
@@ -58,7 +59,7 @@ fig, ax = plt.subplots()
 speeds = np.linspace(0.0, 10.0, num=101)
 kphidots = -8.0*(5.0 - speeds)
 kphidots[50:] = 0.0
-ax = model.plot_eigenvalue_parts(ax=ax, kphidot=kphidots, v=speeds)
+ax = model.plot_eigenvalue_parts(ax=ax, v=speeds, kphidot=kphidots)
 fig.savefig(os.path.join(FIG_DIR, 'leilas-controller-eig-vs-speeds.png'),
             dpi=300)
 
